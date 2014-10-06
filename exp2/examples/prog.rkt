f3838358f67e2e8adbcb71fd95e790ae6b123343
Leif Andersen <leif@leifandersen.net>
2014-10-05 21:59:48 -0400

    Add program that breaks jit.

---
+++ exp2/examples/prog.rkt
@@ -0,0 +1,26 @@
+#lang racket
+
+(require (for-syntax syntax/parse
+                     racket/syntax))
+
+(define (func x)
+  (for ([i (in-range 10000)])
+    (* i i)))
+
+(define-syntax (f stx)
+  (syntax-parse stx
+    [(_ 0 t)
+     #'(void)]
+    [(_ n t)
+     #:when (positive? (syntax->datum #'n))
+     #:with f-n (format-id stx "~a-~a" "f" (syntax->datum #'n))
+     #:with n-1 (- (syntax->datum #'n) 1)
+     #'(begin
+         (define (f-n x)
+           (for ([i (in-range x)])
+             i))
+         (f-n t)
+         (f n-1 t))]))
+
+(time
+ (f 1000 10))
